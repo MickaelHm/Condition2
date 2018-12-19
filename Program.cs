1af8751c1cd1bef2c61ccfd6981413acddb1b9ac
MickaelHm <harmeau.mickael@gmail.com>
2018-12-19 13:52:28 +0100

    Correction

--- Program.cs
+++ Program.cs
@@ -11,14 +11,18 @@ namespace Condition2
         static void Main(string[] args)
         {
             Console.WriteLine("Ecrivez votre âge svp");
-            int age = int.Parse(Console.ReadLine());
-            if (age >= 18)
+            int age = int.Parse(Console.ReadLine());//int.Parse converti string en int
+            if (age >= 18 && age <=120)//vérification que l'age est bien supérieur ou égal à 18 ans et ne dépasse 10 ans
             {
                 Console.WriteLine($"Vous avez {age} ans, vous êtes donc majeur.");
             }
-            else
+            else if (age < 18 && age > 0)// Vérification que l'age est bien compris entre 0 et 18 ans
             {
                 Console.WriteLine($"Vous avez {age} ans, vous êtes donc mineur.");
+            }
+            else
+            {
+                Console.WriteLine("Oops, il doit y avoir une erreur");
             }
         }
     }
